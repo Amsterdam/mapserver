3fb0ba115cc628f25df86f1dec2a662b6a419af5
Edward Mac Gillavry <emacgillavry@gmail.com>
2015-11-06 13:55:46 +0100

    Voorbereiden TIFF bestanden tbv MapServer
    Staat nu al op de server onder /srv/luforecept/

---
+++ luforecept-rd.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# see http://mapserver.org/input/raster.html
+# aanroepen voorbeeld:
+# 
+# ./luforecept-rd.sh /map/lufo/2015
+#
+# Dit script wordt gedaan op een directory waarbinnen in de subdir “tiffsrc” de ruwe GeoTIFF bestanden staan.
+# Het resultaat wordt geplaatst in de subdir “tiffdst” die moet bestaan.
+# de bestanden uit
+# /map/lufo/2015/tiffsrc bewerkt en geplaatst in
+# /srv/mapserver/lufo/2015/.
+
+
+DIR=$1
+cd $DIR
+
+SRC_TIFF_FILES=`/bin/ls tiffsrc/*.tif`
+for SRC_TIFF_FILE in $SRC_TIFF_FILES
+
+do
+echo "START $SRC_TIFF_FILE"
+# basis file naam
+BASE_NAME=`echo $SRC_TIFF_FILE | cut -d/ -f2 | cut -d'.' -f1`
+DEST_TIFF_FILE=/srv/mapserver/lufo/2015/${BASE_NAME}.tif
+/bin/rm -f $DEST_TIFF_FILE
+
+echo "gdal_translate $SRC_TIFF_FILE $DEST_TIFF_FILE"
+
+# De eerste stap comprimeert vaak tot minder dan 10% van de oorspronkelijk omvang en maakt interne tiles aan.
+
+gdal_translate -co TILED=YES -a_srs "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.999908 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs no_defs" -co COMPRESS=JPEG -co PHOTOMETRIC=YCBCR $SRC_TIFF_FILE $DEST_TIFF_FILE
+
+# De tweede creëert gegeneraliseerde niveaus voor als je ver uitzoomt. 
+
+echo "gdaladdo -$DEST_TIFF_FILE"
+
+gdaladdo -ro -r gauss --config COMPRESS_OVERVIEW JPEG --config PHOTOMETRIC_OVERVIEW YCBCR $DEST_TIFF_FILE 2 4 8 16 32 64 128
+
+echo "END $BASE_NAME"
+done
+
+# GeoTIFF-directory is oorspronkelijk 121G. Na conversie met GDAL blijft hier 7G van over!
+
+# Maak een Esri Shape-bestand als index op de definitieve locatie van de GeoTIFFs.
+
+gdaltindex imagery.shp /srv/mapserver/lufo/2015/*.tif
+
+# Plaats Esri shape-bestand bij de lufo's in de map
+# Paden in de toekomst snel wijzigen?
+# - converteer Esri Shape-bestand naar GeoJSON
+# - pas paden in het GeoJSON-bestand aan mbv texteditor
+# - converteer GeoJSON-bestand terug naar Esri Shape-bestand
+#
+# Maak een MapServer configuratie aan:
+# 
+# LAYER
+#  NAME "lufo2015"
+#  TILEINDEX "/srv/mapserver/lufo/2015/imagery.shp"
+#  TILEITEM "location"
+#  TYPE RASTER
+# END
+
+# MapProxy configuratie as usual.. verwijzen naar de juiste WMS endpoint
+# Wel opslaan als JPG-bestanden: veel kleiner en mooier voor lufo's dan PNG-bestanden
