35e5c5c915803859c8faf35728e27f5fba60bb96
pberkhout <patrick.berkhout@trivento.nl>
2017-07-25 10:02:29 +0200

    map server-apache error logging

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -136,6 +136,13 @@ CONNECTIONTYPE postgis
 CONNECTION "host=${OVERLASTGEBIEDEN_DB_HOST} dbname=${OVERLASTGEBIEDEN_DB_NAME} user=${OVERLASTGEBIEDEN_DB_USER} password=${OVERLASTGEBIEDEN_DB_PASSWORD} port=${OVERLASTGEBIEDEN_DB_PORT}"
 EOF
 
+# Configure apache to redirect errors to stderr.
+# The mapserver will redirect errors to apache errorstream (see header.inc and private/header.inc)
+# and apache will then redirect this to stderr, which will then be redirected to syslog/kibana.
+# ref: http://mapserver.org/optimization/debugging.html#steps-to-enable-mapserver-debugging
+#      https://serverfault.com/questions/711168/writing-apache2-logs-to-stdout-stderr
+sed -i 's/ErrorLog .*/ErrorLog \/dev\/stderr/' /etc/apache2/apache2.conf
+
 echo Starting server
 apachectl -D FOREGROUND
 
