61565025fa421c497dfb495a773701ebadc06c74
DonQueso89 <k.van.ekeren@amsterdam.nl>
2022-11-10 15:15:11 +0100

    Script for enforcing max scopes on maps

--- tools/auth_check.py
+++ tools/auth_check.py
@@ -3,7 +3,9 @@
 import argparse
 import glob
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Callable
+from types import SimpleNamespace
+import re
 
 import logging
 import mappyfile as mf
@@ -12,6 +14,7 @@ from mappyfile.parser import Parser
 from mappyfile.transformer import MapfileToDict
 from mappyfile.pprint import PrettyPrinter
 from schematools.utils import dataset_schemas_from_url
+from schematools.types import DatasetSchema
 
 SCHEMA_URL = "https://schemas.data.amsterdam.nl/datasets"
 ACC_SCHEMA_URL = "https://acc.schemas.data.amsterdam.nl/datasets"
@@ -101,7 +104,77 @@ def get_table_identifiers(data_expr: str) -> set[str]:
     return identifiers
 
 
-def run_check(schema_url: str, private_only: bool, include_maps: Optional[list[str]], exclude_maps: Optional[list[str]]):
+SCOPE_OPENBAAR = "OPENBAAR"
+SCOPE_FP_MDW = "FP/MDW"
+
+
+def scope_too_high(scope: str, highest_scope: str) -> bool:
+    ordering = {SCOPE_OPENBAAR: 0, SCOPE_FP_MDW: 1}
+
+    if ordering.get(scope, 999) > ordering[highest_scope]:
+        return True
+
+    return False
+
+azure_pattern = re.compile(".*dbname=mdbdataservices.*")
+cloudvps_pattern = re.compile(".*dbname=dataservices.*")
+
+def is_reference_db_conn(dsn: str) -> bool:
+    return bool(azure_pattern.match(dsn) or cloudvps_pattern.match(dsn))
+
+def auth_from_layer(
+    map_name: str,
+    layer: dict[str, str],
+    schemas: dict[str, DatasetSchema],
+    highest_scope: str,
+) -> list[SimpleNamespace]:
+    """Check if the layer uses tables from the reference database that contain scopes
+    that are too high according to the given highest_scope, and if so, dump the auth info in
+    a namespace describing the table and field level auth scopes that are too high."""
+    # The default connectiontype is local, in which case we are consuming from a
+    # local shapefile
+    if "connectiontype" not in layer or layer["connectiontype"].lower() != "postgis":
+        return []
+    if "connection" not in layer or not is_reference_db_conn(layer["connection"]):
+        return []
+
+    data_expr = layer["data"][0]
+    logger.debug("Data definition for layer %s: \n\n %s", layer.get("name"), data_expr)
+
+    auth_data = []
+    # brute force it
+    for dataset in schemas.values():
+        for table in dataset.tables:
+            if table.db_name in data_expr:
+                table_auth = SimpleNamespace(
+                    map_=map_name,
+                    layer=layer["name"],
+                    table=table.db_name,
+                    table_scopes=set(),
+                    field_scopes=[],
+                    max_scope=highest_scope
+                )
+                for scope in dataset.auth:
+                    if scope_too_high(scope, highest_scope):
+                        table_auth.table_scopes.add(scope)
+
+                for f in table.fields:
+                    for scope in f.auth:
+                        if scope_too_high(scope, highest_scope):
+                            table_auth.field_scopes.append((f.db_name, scope))
+
+                if len(table_auth.table_scopes) or len(table_auth.field_scopes):
+                    auth_data.append(table_auth)
+
+    return auth_data
+
+
+def run_check(
+    schema_url: str,
+    private_only: bool,
+    include_maps: Optional[list[str]],
+    exclude_maps: Optional[list[str]],
+):
     printer = PrettyPrinter()
 
     public_maps = sorted(glob.glob(str(repo_root / f"*.map")))
@@ -113,9 +186,8 @@ def run_check(schema_url: str, private_only: bool, include_maps: Optional[list[s
     logger.info("Loading schemas from %s", schema_url)
     schemas = dataset_schemas_from_url(schema_url)
 
-    private_scopes = set()
+    prohibited_queries = []
 
-    # private maps must not contain scops higher than FP/MDW
     for fname in private_maps:
         if include_maps and Path(fname).name.removesuffix(".map") not in include_maps:
             continue
@@ -127,32 +199,19 @@ def run_check(schema_url: str, private_only: bool, include_maps: Optional[list[s
         logger.debug("Parsed map: \n%s", printer.pprint(map_))
 
         for layer in map_["layers"]:
-            if "connectiontype" not in layer or layer["connectiontype"].lower() != "postgis":
-                continue
+            # private maps must not contain scopes higher than FP/MDW
+            prohibited_queries.extend(auth_from_layer(map_["name"], layer, schemas, SCOPE_FP_MDW))
+    if prohibited_queries:
 
-            data_expr = layer["data"][0]
-            logger.debug(
-                "Data definition for layer %s: \n\n %s", layer.get("name"), data_expr
-            )
-
-            # brute force it
-            for dataset in schemas.values():
-                for table in dataset.tables:
-                    if table.db_name in data_expr:
-                        private_scopes |= table.auth
-                        private_scopes |= dataset.auth
-                        for f in table.fields:
-                            private_scopes |= f.auth
-    
-    logger.info(
-        "Found the following scopes in private maps: \n\n - %s", "\n - ".join(private_scopes)
-    )
+        logger.info(
+            "Found the following queries in private maps that are possibly breaching auth requirements: \n\n - %s",
+            "\n - ".join(map(str, prohibited_queries)),
+        )
 
     if private_only:
         return
 
-    public_scopes = set()
-    # public maps must not contain scopes higher than OPENBAAR
+    prohibited_queries = []
     for fname in public_maps:
         if include_maps and Path(fname).name.removesuffix(".map") not in include_maps:
             continue
@@ -164,26 +223,14 @@ def run_check(schema_url: str, private_only: bool, include_maps: Optional[list[s
         logger.debug("Parsed map: \n%s", printer.pprint(map_))
 
         for layer in map_["layers"]:
-            if "connectiontype" not in layer or layer["connectiontype"].lower() != "postgis":
-                continue
+            # public maps must not contain scopes higher than OPENBAAR
+            prohibited_queries.extend(auth_from_layer(map_["name"], layer, schemas, SCOPE_OPENBAAR))
 
-            data_expr = layer["data"][0]
-            logger.debug(
-                "Data definition for layer %s: \n\n %s", layer.get("name"), data_expr
-            )
-
-            # brute force it
-            for dataset in schemas.values():
-                for table in dataset.tables:
-                    if table.db_name in data_expr:
-                        public_scopes |= table.auth
-                        public_scopes |= dataset.auth
-                        for f in table.fields:
-                            public_scopes |= f.auth
-    
-    logger.info(
-        "Found the following scopes in public maps: \n\n - %s", "\n - ".join(public_scopes)
-    )
+    if prohibited_queries:
+        logger.info(
+            "Found the following queries in pulic maps that are possibly breaching auth requirements: \n\n - %s",
+            "\n - ".join(map(str, prohibited_queries)),
+        )
 
 
 if __name__ == "__main__":
