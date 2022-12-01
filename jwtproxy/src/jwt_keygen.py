1c7b8d3f4b14c12a2861ae02ff51b3d8ff7dbefe
DonQueso89 <k.van.ekeren@amsterdam.nl>
2022-12-01 18:35:33 +0100

    poc jwtproxy

---
+++ jwtproxy/src/jwt_keygen.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import argparse
+import json
+import pathlib
+import sys
+import time
+import uuid
+
+from jwcrypto.jwk import JWK
+from jwcrypto.jwt import JWT
+
+"""
+Utitility script for generating JSON web keys and JSON web tokens
+which can be used when testing. JWTs can contain private
+claims which are scopes as used in the authorization mechanism of
+amsterdam-schema.
+
+The key used by the development configuration of the jwtproxy is stored in
+./test_jwk.json.
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "scopes", nargs="*", help="Scopes added to the JWT as private claims"
+)
+parser.add_argument(
+    "--exp", default=None, type=int, help="Number of seconds to expiration"
+)
+
+JWK_PATH = pathlib.Path(__file__).parent / "test_jwk.json"
+
+
+def generate_jwk():
+    return JWK.generate(
+        kty="EC", crv="P-256", kid=str(uuid.uuid4()), key_ops=["verify", "sign"]
+    )
+
+
+def generate_jwt(scopes, exp):
+    """Generate a JWT, signed using the first key stored at test_jwk.json.
+    If there is no JWKSet at this location, generate it and write it to
+    the file as a JWKSet"""
+    if JWK_PATH.exists():
+        key = JWK(**json.load(open(JWK_PATH))["keys"][0])
+        if "d" not in key:
+            raise ValueError("The configured JWK does not contain a private key")
+    else:
+        with open(JWK_PATH, "w+") as fp:
+            key = generate_jwk()
+            json.dump({"keys": [key]}, fp, indent=2, sort_keys=True)
+
+    scopes = list(set(scopes))
+    now = int(time.time())
+    claims = {
+        "iat": now,
+        "scopes": scopes,
+        "sub": "test@tester.nl",
+    }
+    if exp is not None:
+        claims["exp"] = now + exp
+
+    token = JWT(header={"alg": "ES256", "kid": key.kid, "kty": "EC"}, claims=claims)
+    token.make_signed_token(key)
+
+    sys.stdout.write(token.serialize())
+    sys.stdout.write("\n")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    generate_jwt(args.scopes, args.exp)
---
+++ jwtproxy/src/server.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import logging
+from os import environ as env
+
+import argparse
+from aiohttp import ClientSession, web
+from jwcrypto.jwt import JWT, JWKSet
+from yarl import URL
+
+
+logger = logging.getLogger("aiohttp.server.jwtproxy")
+
+CHUNK_SIZE = 1024
+
+def get_jwk():
+    # https://login.microsoftonline.com/{tenant}/discovery/v2.0/keys
+    return JWKSet.from_json(open(env.get("JWKS_PATH")).read())
+
+async def handle(req):
+    path = req.match_info["path"]
+    target_url = URL("".join([env["PROXY_URL"].rstrip("/"), "/", path.lstrip("/")]))
+
+    logger.info("Proxy-URL: \n%s", target_url)
+    logger.debug("Request params: \n %s", req.rel_url.query)
+    logger.debug("Request headers: \n %s", req.headers)
+
+    try:
+        auth = req.headers["Authorization"]
+    except KeyError:
+        # TODO: Redirect to AAD /oauth/v2.0/authorize
+        raise web.HTTPForbidden(reason="No JWT present in Authorization header")
+
+    bearer = "bearer "
+    if not auth[:len(bearer)].lower() == bearer:
+        raise web.HTTPForbidden(reason="Authorization header format is 'Bearer <token>'")
+
+    token = auth[len(bearer):]
+    j = JWT()
+
+    try:
+        j.deserialize(token, key=get_jwk())
+    except Exception as e:
+        logger.warning(e)
+        raise web.HTTPForbidden(reason="Invalid token")
+    
+    logger.debug("Token: %s", repr(j))
+
+    async with ClientSession() as session:
+        async with session.request(
+            req.method, target_url, headers=req.headers, params=req.rel_url.query
+        ) as resp:
+            logger.info("Response status from mapserver: %s", resp.status)
+            logger.debug("Headers from mapserver: \n %s", resp.headers)
+            logger.debug("Params from mapserver: \n %s", req.rel_url.query)
+
+            server_resp = web.StreamResponse(headers=resp.headers, status=resp.status)
+
+            if resp.headers.get("Transfer-Encoding", None) == "chunked":
+                server_resp.enable_chunked_encoding()
+            if resp.headers.get("Content-Encoding") == "gzip":
+                server_resp.enable_compression()
+
+            await server_resp.prepare(req)
+            async for chunk in resp.content.iter_chunked(CHUNK_SIZE):
+                await server_resp.write(chunk)
+            await server_resp.write_eof()
+            return server_resp
+
+parser = argparse.ArgumentParser(description="jwt-proxy server")
+parser.add_argument('--path')
+parser.add_argument('--port', default=8080, type=int)
+
+
+async def main(*argv):
+    logging.basicConfig(level=env.get("LOG_LEVEL", logging.INFO))
+
+    app = web.Application()
+    app.router.add_route("OPTIONS", "/{path:.*?}", handle)
+    app.router.add_route("GET", "/{path:.*?}", handle)
+    app.router.add_route("POST", "/{path:.*?}", handle)
+    return app
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    web.run_app(main(), host="0.0.0.0", path=args.path)
