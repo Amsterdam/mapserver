afc1841e499ac20976ac836751207a3eeb962869
LB <78799392+lbam@users.noreply.github.com>
2023-04-26 07:06:02 +0200

    Minor cleanup of generating code and .dockerignore it

--- generate.py
+++ generate.py
@@ -1,30 +1,36 @@
+# Common code for mapfile generation scripts.
+
 from contextlib import contextmanager
 
-indent: int = 0
+
+_indent: int = 0
 
 
 def p(*strs: list[str]):
-    print("  " * indent, end="")
+    """Prints strs, all on one line, with all but the first quoted."""
+    print("  " * _indent, end="")
     print(strs[0], end="")
     if len(strs) > 1:
         print(" ", end="")
-    print_quoted(strs[1:])
+    _print_quoted(strs[1:])
 
 
 def q(*strs: list[str]):
-    print("  " * indent, end="")
-    print_quoted(strs)
+    """Prints strs, all on one line, all quoted."""
+    print("  " * _indent, end="")
+    _print_quoted(strs)
 
 
-def print_quoted(strs: list[str]):
+def _print_quoted(strs: list[str]):
     print(*map(repr, strs))
 
 
 @contextmanager
 def block(typ: str):
+    """Introduces an indented block of type typ. The block is closed with END."""
     p(typ)
-    global indent
-    indent += 1
+    global _indent
+    _indent += 1
     yield
-    indent -= 1
+    _indent -= 1
     p("END")
