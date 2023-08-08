267f453f19349c0fd9a40ba64bf9dc73f716fa8e
LB <78799392+lbam@users.noreply.github.com>
2023-08-08 11:52:19 +0200

    Add dataset vegetatieobjecten_bomen

---
+++ gen_vegetatieobjecten_bomen.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+from generate import block, p, q
+
+
+species = [
+    "Iep (Ulmus)",
+    "Linde (Tilia)",
+    "Esdoorn (Acer)",
+    "Es (Fraxinus)",
+    "Plataan (Platanus)",
+    "Eik (Quercus)",
+    "Populier (Populus)",
+    "Els (Alnus)",
+    "Wilg (Salix)",
+    "Berk (Betula)",
+    "Kers (Prunus)",
+    "Haagbeuk (Carpinus)",
+    "Meidoorn (Crataegus)",
+    "Acacia (Robinia)",
+    "Paardenkastanje (Aesculus)",
+    "Vleugelnoot (Pterocarya)",
+]
+
+layers = [name for name in species] + ["Onbekende soorten", "Overige soorten"]
+
+print("# GENERATED FILE, DO NOT EDIT.\n\n")
+print("# TEAM: BOR\n")
+
+with block("MAP"):
+    p("NAME", "vegetatieobjecten")
+    p("INCLUDE", "header.inc")
+    p("DEBUG", 5)
+
+    with block("WEB"):
+        with block("METADATA"):
+            q("ows_title", "Vegetatieobjecten")
+            q(
+                "ows_abstract",
+                "Dataset met actuele informatie over de bomen in beheer van gemeente Amsterdam",
+            )
+            q("wms_extent", "100000 450000 150000 500000")
+
+    for name in layers:
+        with block("LAYER"):
+            p("NAME", name)
+            p("GROUP", "bomen")
+
+            with block("PROJECTION"):
+                q("init=epsg:28992")
+
+            p("INCLUDE", "connection/dataservices.inc")
+            if name == "Onbekende soorten":
+                select_soort = "soortnaam_top = 'Onbekend' OR soortnaam_top IS NULL"
+            elif name == "Overige soorten":
+                select_soort = (
+                    " AND ".join(f"soortnaam_top != '{s}'" for s in species)
+                    + " AND soortnaam_top IS NOT NULL"
+                )
+            else:
+                select_soort = f"soortnaam_top = '{name}'"
+            p(
+                "DATA",
+                "geometrie FROM ("
+                "   SELECT * FROM public.bomen_stamgegevens"
+                "   WHERE type_soortnaam = 'Bomen'"
+                "   AND type_eigenaar_plus = 'Gemeente Amsterdam'"
+                f"  AND ({select_soort})"
+                ") AS sub"
+                " USING srid=28992"
+                " USING UNIQUE id",
+            )
+            p("TYPE POINT")
+
+            with block("METADATA"):
+                q("wfs_enable_request", "none")
+                q("wms_title", name)
+                q("wms_enable_request", "*")
+                q("wms_srs", "EPSG:28992")
+                q("wms_name", "Bomen")
+                q("wms_format", "image/png")
+                q("wms_server_version", "1.3.0")
+                q("wms_extent", "100000 450000 150000 500000")
+
+            p("LABELITEM", "soortnaam_top")
+            p("CLASSITEM", "soortnaam_top")
+
+            with block("CLASS"):
+                p("NAME", name)
+
+                with block("STYLE"):
+                    p("SYMBOL", f"bomen_{name.split()[0].lower()}")
+                    p("SIZE", 16)
