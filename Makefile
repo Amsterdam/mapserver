56c21629c3179afd69b08404085a97171595d0a8
DonQueso89 <k.van.ekeren@amsterdam.nl>
2023-09-21 11:25:23 +0200

    Remove topografie MAP_URL replace

--- Makefile
+++ Makefile
@@ -3,4 +3,6 @@ install-deps:
 run-auth-check: install-deps
 	@# bgt_inrichtingselementen is known to fail due to
 	@# a flaw in the mappyfile mapfile grammar
-	./tools/auth_check.py -e bgt_inrichtingselementen
+	@# addresseerbare_objecten fails because the mappyfile mapfile grammar
+	@# does not support the BINDVALS directive
+	./tools/auth_check.py -e bgt_inrichtingselementen adresseerbare_objecten
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -118,10 +118,6 @@ fi;
 sed -i 's/ErrorLog .*/ErrorLog \/dev\/stderr/' /etc/apache2/apache2.conf
 sed -i 's/Timeout 300/Timeout 600/' /etc/apache2/apache2.conf
 
-# Replace actual location of the mapserver depending on the environment
-sed -i 's#MAP_URL_REPLACE#'"$MAP_URL"'#g' /srv/mapserver/topografie.map /srv/mapserver/topografie_wm.map /srv/mapserver/lufo.map /srv/mapserver/infrarood.map
-sed -i 's#LEGEND_URL_REPLACE#'"$LEGEND_URL"'#g' /srv/mapserver/topografie.map /srv/mapserver/topografie_wm.map
-
 mkdir -p /srv/mapserver/config
 # python3 /srv/mapserver/tools/make_mapfile_config.py > /srv/mapserver/sld/config.json
 
