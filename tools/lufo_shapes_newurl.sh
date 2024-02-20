88a36f321c10fc84a4fd94e4f7b5465cf78c90cb
LB <78799392+lbam@users.noreply.github.com>
2024-02-20 10:18:12 +0100

    Move lufo_shapes_newurl.sh to tools and clean it up

--- tools/lufo_shapes_newurl.sh
+++ tools/lufo_shapes_newurl.sh
@@ -26,24 +26,20 @@ SHAPEDIR=$SCRIPTDIR/luchtfoto
 LEVELS="0 1 2 3 4 5"
 YEAR="2018 2017 2016 2015 2014 2013 2012 2011 2010 2009 2008 2007 2006 2005 2004 2003"
 
-for j in $YEAR; do for i in $LEVELS; do
-
-    echo "DOING $j $i"
-
-    ogrinfo -dialect SQLITE -sql "UPDATE 'imagery-$j-$i' SET location = '/vsicurl/https://files.' || SUBSTR(location, 18)" $SHAPEDIR/imagery-$j-$i.shp
-
-    ogrinfo -sql "CREATE SPATIAL INDEX ON imagery-$j-$i" $SHAPEDIR/imagery-$j-$i.shp
-
-done; done
+for j in $YEAR; do
+    for i in $LEVELS; do
+        ogrinfo -dialect SQLITE -sql "UPDATE 'imagery-$j-$i' SET location = '/vsicurl/https://files.' || SUBSTR(location, 18)" $SHAPEDIR/imagery-$j-$i.shp
+        ogrinfo -sql "CREATE SPATIAL INDEX ON imagery-$j-$i" $SHAPEDIR/imagery-$j-$i.shp
+    done
+done
 
 SHAPEDIR=$SCRIPTDIR/infrarood
 LEVELS="0 1 2 3 4 5"
 YEAR="2018"
 
-for j in $YEAR; do for i in $LEVELS; do
-
-    ogrinfo -dialect SQLITE -sql "UPDATE 'imagery-$j-$i' SET location = '/vsicurl/https://files.' || SUBSTR(location, 18)" $SHAPEDIR/imagery-$j-$i.shp
-
-    ogrinfo -sql "CREATE SPATIAL INDEX ON imagery-$j-$i" $SHAPEDIR/imagery-$j-$i.shp
-
-done; done
+for j in $YEAR; do
+    for i in $LEVELS; do
+        ogrinfo -dialect SQLITE -sql "UPDATE 'imagery-$j-$i' SET location = '/vsicurl/https://files.' || SUBSTR(location, 18)" $SHAPEDIR/imagery-$j-$i.shp
+        ogrinfo -sql "CREATE SPATIAL INDEX ON imagery-$j-$i" $SHAPEDIR/imagery-$j-$i.shp
+    done
+done
