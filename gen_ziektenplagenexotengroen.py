7bd0df9c3413772a025f6abfa9d9292ee575918e
dolenburgamsterdam <d.oldenburg@amsterdam.nl>
2024-05-15 15:40:23 +0200

    Toevoegen JPD aan Ziekteplangeexotengroen

--- gen_ziektenplagenexotengroen.py
+++ gen_ziektenplagenexotengroen.py
@@ -23,8 +23,19 @@ layers_EPR = [
 ]
 
 layers_JPD = [
-    ("Japanse Duizendknoop Meldingen","Japanse Duizendknoop Meldingen Gemeld", "Gemeld", "gfe"),
-    ("Japanse Duizendknoop Meldingen","Japanse Duizendknoop Meldingen Duizendknoop verwijderd", "Duizendknoop verwijderd", "brood")
+    ("Japanse Duizendknoop Meldingen", "Gemeld", "JPD_Gemeld"),
+    ("Japanse Duizendknoop Meldingen", "Duizendknoop aanwezig, niet bereikbaar", "JPD_Duizendknoop_aanwezig_niet_bereikbaar"),
+    ("Japanse Duizendknoop Meldingen", "Duizendknoop aanwezig", "JPD_Duizendknoop_aanwezig"),
+    ("Japanse Duizendknoop Meldingen", "In bestrijding", "JPD_In_bestrijding"),
+    ("Japanse Duizendknoop Meldingen","Monitoring", "JPD_Monitoring"),
+    ("Japanse Duizendknoop Meldingen", "Duizendknoop verwijderd", "JPD_Duizendknoop_verwijderd"),
+
+    ("Japanse Duizendknoop Inspecties", "Gemeld", "#004698"),
+    ("Japanse Duizendknoop Inspecties", "Duizendknoop aanwezig, niet bereikbaar", "#767676"),
+    ("Japanse Duizendknoop Inspecties", "Duizendknoop aanwezig", "#ec0000"),
+    ("Japanse Duizendknoop Inspecties", "In bestrijding", "#fe9100"),
+    ("Japanse Duizendknoop Inspecties","Monitoring", "#029ce6"),
+    ("Japanse Duizendknoop Inspecties", "Duizendknoop verwijderd", "#00a03c")
 ]
 
 header("Bor / Beeldschoon")
@@ -85,9 +96,9 @@ with block("MAP"):
 
     
     #Vanaf hier is het voor de japanse duizendknoop
-    for group, name, filter, icon in layers_JPD:
+    for group, filter, icon_color in layers_JPD:
         with block("LAYER"):
-            p("NAME", slugify(name))
+            p("NAME", slugify(filter))
             p("GROUP", slugify(group))
 
             with block("PROJECTION"):
@@ -95,32 +106,61 @@ with block("MAP"):
 
             p("INCLUDE", "connection/dataservices.inc")
             
+    #hier voor de meldingen
             if 'Meldingen' in group:
                 p(
                     "DATA",
                     "geometrie FROM public.ziekte_plagen_exoten_groen_japanseduizendknoop_meldingen USING srid=28992 USING UNIQUE id"
                 )
-            else:
+                p("TYPE POINT")
+
+                p("TEMPLATE", "fooOnlyForWMSGetFeatureInfo.html")
+                p("CLASSITEM", "status_kaartlaag")
+                # kleine aanpassing aan de print structuur om te voorkomen dat er '' om deze komt.
+                print (f'FILTER ("[status_kaartlaag]" = "{filter}")')
+                
+                with block("METADATA"):
+                    q("ows_title", filter)
+                    q("wms_enable_request", "*")
+                    q("wms_abstract", "Japanse Duizendknoop Amsterdam")
+                    q("wms_srs", "EPSG:28992")
+                    q("ows_group_title", group)
+
+                with block("CLASS"):
+                    p("NAME", filter)
+
+                    with block("STYLE"):
+                        p("SYMBOL", icon_color)
+                        p("SIZE", 20)
+
+    #hier voor de inspecties    
+            if 'Inspecties' in group:
                 p(
                     "DATA",
                     "geometrie FROM public.ziekte_plagen_exoten_groen_japanseduizendknoop_inspecties USING srid=28992 USING UNIQUE id"
                 )
-            p("TYPE POINT")
-            p("TEMPLATE", "fooOnlyForWMSGetFeatureInfo.html")
-            p("CLASSITEM", "status_kaartlaag")
-            # kleine aanpassing aan de print structuur om te voorkomen dat er '' om deze komt.
-            print (f'FILTER ("[status_kaartlaag]" = "{filter}")')
-            
-            with block("METADATA"):
-                q("ows_title", name)
-                q("wms_enable_request", "*")
-                q("wms_abstract", "Japanse Duizendknoop Amsterdam")
-                q("wms_srs", "EPSG:28992")
-                q("ows_group_title", group)
+                p("TYPE POLYGON")
 
-            with block("CLASS"):
-                p("NAME", name)
+                p("TEMPLATE", "fooOnlyForWMSGetFeatureInfo.html")
+                p("CLASSITEM", "status_kaartlaag")
+                # kleine aanpassing aan de print structuur om te voorkomen dat er '' om deze komt.
+                print (f'FILTER ("[status_kaartlaag]" = "{filter}")')
+                
+                with block("METADATA"):
+                    q("ows_title", filter)
+                    q("wms_enable_request", "*")
+                    q("wms_abstract", "Japanse Duizendknoop Amsterdam")
+                    q("wms_srs", "EPSG:28992")
+                    q("ows_group_title", group)
 
-                with block("STYLE"):
-                    p("SYMBOL", icon)
-                    p("SIZE", 20)
+                with block("CLASS"):
+                    p("NAME", filter)
+
+                    with block("STYLE"):
+
+                        p("COLOR", icon_color)
+                        p("OPACITY", 20) 
+
+                    with block("STYLE"):
+                        p("OUTLINECOLOR ", icon_color)
+                        p("WIDTH ", 2)
