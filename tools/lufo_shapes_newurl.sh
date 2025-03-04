e22d02b36def9452f05b58ef0f8c1c9bb8e27d02
Davey Oldenburg <d.oldenburg@amsterdam.nl>
2025-03-04 16:15:57 +0100

    WIP: indexen maken, mapfile aanmaken 2024 en locatie van TIF toevoegen.

--- tools/lufo_shapes_newurl.sh
+++ tools/lufo_shapes_newurl.sh
@@ -19,27 +19,27 @@
 
 set -u # crash on missing env
 set -e # stop on any error
-
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+#It's looking for the shapefiles in this folder, there in the main mapserver folder, you can move this file or move the shp files into this folder
 SHAPEDIR=$SCRIPTDIR/luchtfoto
 LEVELS="0 1 2 3 4 5"
-YEAR="2018 2017 2016 2015 2014 2013 2012 2011 2010 2009 2008 2007 2006 2005 2004 2003"
+YEAR="2024"
 
 for j in $YEAR; do
     for i in $LEVELS; do
-        ogrinfo -dialect SQLITE -sql "UPDATE 'imagery-$j-$i' SET location = '/vsicurl/https://files.' || SUBSTR(location, 18)" $SHAPEDIR/imagery-$j-$i.shp
+        ogrinfo -dialect SQLITE -sql "UPDATE 'imagery-$j-$i' SET location = '/vsicurl/https://stlandingdpgoontweu01.blob.core.windows.net.' || SUBSTR(location, 18)" $SHAPEDIR/imagery-$j-$i.shp
         ogrinfo -sql "CREATE SPATIAL INDEX ON imagery-$j-$i" $SHAPEDIR/imagery-$j-$i.shp
     done
 done
 
-SHAPEDIR=$SCRIPTDIR/infrarood
-LEVELS="0 1 2 3 4 5"
-YEAR="2018"
+# SHAPEDIR=$SCRIPTDIR/infrarood
+# LEVELS="0 1 2 3 4 5"
+# YEAR="2018"
 
-for j in $YEAR; do
-    for i in $LEVELS; do
-        ogrinfo -dialect SQLITE -sql "UPDATE 'imagery-$j-$i' SET location = '/vsicurl/https://files.' || SUBSTR(location, 18)" $SHAPEDIR/imagery-$j-$i.shp
-        ogrinfo -sql "CREATE SPATIAL INDEX ON imagery-$j-$i" $SHAPEDIR/imagery-$j-$i.shp
-    done
-done
+# for j in $YEAR; do
+#     for i in $LEVELS; do
+#         ogrinfo -dialect SQLITE -sql "UPDATE 'imagery-$j-$i' SET location = '/vsicurl/https://stlandingdpgoontweu01.blob.core.windows.net.' || SUBSTR(location, 18)" $SHAPEDIR/imagery-$j-$i.shp
+#         ogrinfo -sql "CREATE SPATIAL INDEX ON imagery-$j-$i" $SHAPEDIR/imagery-$j-$i.shp
+#     done
+# done
