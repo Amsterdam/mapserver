75c142c73072676dd4c2169c072918b5cbf8c97e
Davey <145351251+Doldenburgamsterdam@users.noreply.github.com>
2025-09-16 12:09:20 +0200

    Hier moet de private file worden toegevoegd anders kan hij hem niet vinden... (#681)

--- jwtproxy/src/server.py
+++ jwtproxy/src/server.py
@@ -17,7 +17,7 @@ LOG_LEVEL = env.get("LOG_LEVEL", "INFO")
 CHUNK_SIZE = 1024
 CONN_POOL_SIZE = 100
 AZURE_APPLICATIONINSIGHTS_CONNSTRING = env.get("AZURE_APPLICATIONINSIGHTS_CONNSTRING")
-PRIVATE_MAPS = env.get("PRIVATE_MAPS", "blackspots brandkranen handelsregister hr")
+PRIVATE_MAPS = env.get("PRIVATE_MAPS", "blackspots brandkranen handelsregister hr hr_kvk")
 _map_paths = "|".join(f"maps/{m}" for m in PRIVATE_MAPS.split())
 PRIVATE_MAPS_RE_PATTERN = re.compile(f"^({_map_paths})(/|$)")
 
